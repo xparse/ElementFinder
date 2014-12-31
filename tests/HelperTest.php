eb67803e2520bf4cb30f7e4ad2f4cfba0804c5ce
Ivan Scherbak <dev@funivan.com>
2014-12-31 22:32:37 +0200

    Improve form test

--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -13,14 +13,19 @@
         <div>
         <form >
           <input type="text" name="test" value="123"/>
+          <select name="st">
+          <option value="1">1</option>
+          <option value="2" selected="selected">2</option>
+          </select>
         </form>
         </div>
       
       ';
       $page = new \Xparse\ElementFinder\ElementFinder($html);
       $formData = \Xparse\ElementFinder\Helper::getDefaultFormData($page, '//form');
-      $this->assertCount(1, $formData);
+      $this->assertCount(2, $formData);
       $this->assertEquals(123, $formData['test']);
+      $this->assertEquals(2, $formData['st']);
 
     }
   }
