14412cee4c2e4f0327956234989174b28c975dd2
Ivan Scherbak <dev@funivan.com>
2017-11-02 10:54:23 +0200

    #89 add php_cs_fixer

---
+++ .php_cs_config.php
@@ -0,0 +1,29 @@
+<?php
+$config = PhpCsFixer\Config::create()
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR2' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'blank_line_before_statement' => true,
+        'strict_comparison' => true,
+        'strict_param' => true,
+        'yoda_style' => false,
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+          ->in(__DIR__.'/src')
+          ->in(__DIR__.'/tests')
+    )
+;
+// special handling of fabbot.io service if it's using too old PHP CS Fixer version
+try {
+    PhpCsFixer\FixerFactory::create()
+        ->registerBuiltInFixers()
+        ->registerCustomFixers($config->getCustomFixers())
+        ->useRuleSet(new PhpCsFixer\RuleSet($config->getRules()));
+} catch (PhpCsFixer\ConfigurationException\InvalidConfigurationException $e) {
+    $config->setRules([]);
+} catch (UnexpectedValueException $e) {
+    $config->setRules([]);
+}
+return $config;
