a4f5b0a3e7421e6cb7b6e239e5f93b808e2649d8
Ivan Scherbak <dev@funivan.com>
2018-04-18 10:32:46 +0300

    fix code style

--- src/ElementFinder/ElementFinderModifierInterface.php
+++ src/ElementFinder/ElementFinderModifierInterface.php
@@ -1,18 +1,18 @@
-<?
+<?php
 
   declare(strict_types=1);
 
   namespace Xparse\ElementFinder\ElementFinder;
 
-  /**
+/**
    * @author Ivan Scherbak <alotofall@gmail.com>
    */
-  interface ElementFinderModifierInterface {
+  interface ElementFinderModifierInterface
+  {
 
     /**
      * @param \DOMNodeList $nodeList
      * @return void
      */
-    public function modify(\DOMNodeList $nodeList);
-
+      public function modify(\DOMNodeList $nodeList);
   }
--- src/ElementFinder/RemoveElements.php
+++ src/ElementFinder/RemoveElements.php
@@ -4,19 +4,21 @@
 
   namespace Xparse\ElementFinder\ElementFinder;
 
-  class RemoveElements implements ElementFinderModifierInterface {
+class RemoveElements implements ElementFinderModifierInterface
+{
 
     /**
      * @param \DOMNodeList $nodeList
      * @return void
      */
-    public function modify(\DOMNodeList $nodeList) {
-      foreach ($nodeList as $node) {
-        if ($node instanceof \DOMAttr) {
-          $node->ownerElement->removeAttribute($node->name);
-        } else {
-          $node->parentNode->removeChild($node);
+    public function modify(\DOMNodeList $nodeList)
+    {
+        foreach ($nodeList as $node) {
+            if ($node instanceof \DOMAttr) {
+                $node->ownerElement->removeAttribute($node->name);
+            } else {
+                $node->parentNode->removeChild($node);
+            }
         }
-      }
     }
-  }
+}
