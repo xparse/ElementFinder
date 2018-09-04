c0a11e339ad8dd64a70c00843b7424329bb6a0b0
Anastasia Kravets <anastasiakravets@icloud.com>
2018-09-04 10:00:08 +0300

    change phpdoc to ElementFinderInterface

--- src/ElementFinderInterface.php
+++ src/ElementFinderInterface.php
@@ -34,7 +34,7 @@ interface ElementFinderInterface
      * ```
      *
      * @param string $expression
-     * @return ElementFinder
+     * @return ElementFinderInterface
      */
     public function remove($expression) : ElementFinderInterface;
 
