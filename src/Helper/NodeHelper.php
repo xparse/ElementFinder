f4a9f4f663bf74529e23ddfc45806af893e1766e
Vitalii Sevastianov <sevikv@gmail.com>
2022-02-04 14:00:46 +0200

    do not use the `saveXml` method if the current document type is HTML

--- src/Helper/NodeHelper.php
+++ src/Helper/NodeHelper.php
@@ -4,30 +4,32 @@ declare(strict_types=1);
 
 namespace Xparse\ElementFinder\Helper;
 
+use Xparse\ElementFinder\ElementFinder;
+
 /**
  * @author Ivan Shcherbak <alotofall@gmail.com>
  */
 class NodeHelper
 {
-    final public static function getOuterContent(\DOMNode $node): string
+    final public static function getOuterContent(\DOMNode $node, int $documentType): string
     {
         $domDocument = new \DOMDocument('1.0');
         $b = $domDocument->importNode($node->cloneNode(true), true);
         /** @noinspection UnusedFunctionResultInspection */
         $domDocument->appendChild($b);
 
-        $content = $domDocument->saveHTML();
+        $content = $documentType === ElementFinder::DOCUMENT_XML ? $domDocument->saveXml() : $domDocument->saveHTML();
         $content = StringHelper::safeEncodeStr($content);
 
         return $content;
     }
 
 
-    final public static function getInnerContent(\DOMNode $itemObj): string
+    final public static function getInnerContent(\DOMNode $itemObj, int $documentType): string
     {
         $content = '';
         foreach ($itemObj->childNodes as $child) {
-            $content .= $child->ownerDocument->saveXML($child);
+            $content .= ($documentType === ElementFinder::DOCUMENT_XML ? $child->ownerDocument->saveXml($child) : $child->ownerDocument->saveHTML($child));
         }
         return StringHelper::safeEncodeStr($content);
     }
