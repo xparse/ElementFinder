36cfbc97c3ab42a5ed76c37bf35ef99e651b8b0b
Ivan Shcherbak <alotofall@gmail.com>
2024-01-07 23:13:37 +0200

    add rector config

---
+++ rector.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\CodingStyle\Rector\Use_\SeparateMultiUseImportsRector;
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __FILE__]);
+
+    // register a single rule
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+    $rectorConfig->rule(TypedPropertyFromStrictConstructorRector::class);
+    $rectorConfig->rule(SeparateMultiUseImportsRector::class);
+    $rectorConfig->importNames();
+    $rectorConfig->sets([LevelSetList::UP_TO_PHP_80, PHPUnitSetList::PHPUNIT_90]);
+};
