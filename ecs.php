80fd0b50c50810b50fc981a934e726d35acc561f
Ivan Shcherbak <alotofall@gmail.com>
2024-01-11 10:56:12 +0200

    Run Code style check in CI

---
+++ ecs.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\FunctionNotation\VoidReturnFixer;
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __FILE__,
+    ]);
+
+    $ecsConfig->rules([NoUnusedImportsFixer::class, VoidReturnFixer::class]);
+
+    // this way you can add sets - group of rules
+    $ecsConfig->sets([SetList::SPACES, SetList::ARRAY, SetList::DOCBLOCK, SetList::NAMESPACES, SetList::COMMENTS, SetList::PSR_12]);
+};
